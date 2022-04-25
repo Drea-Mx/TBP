6d10d40d2d5f65802d9fcc3aa5fdeb7f2a431f42
Luis B <luisbttf@gmail.com>
2022-04-24 21:19:37 -0500

    gatsby

---
+++ gatsby/gatsby-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  siteMetadata: {
+    title: `The Branding People`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-sanity',
+    options: {
+      "projectId": "v7y3u1wl",
+      "dataset": "production"
+    }
+  }, "gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/assets/images/icon.png"
+    }
+  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/assets/images/"
+    },
+    __key: "images"
+  }]
+};
