899c4b9d88c74c6e5dab9b32c5ba693201a2fd17
Luis B <luisbttf@gmail.com>
2022-05-16 15:09:00 -0500

    project

--- gatsby/src/templates/Project.js
+++ gatsby/src/templates/Project.js
@@ -16,7 +16,7 @@ export default function SingleMezcalPage({ data: { project } }) {
         const { className, onClick } = props;
         return (
           <button className={className} onClick={onClick} onKeyDown={onClick}>
-            <img src='/arrow-r.png' alt='arrow button right' />
+            <img src='/Right_ arrow.png' alt='arrow button right' />
           </button>
         );
       }
@@ -25,7 +25,7 @@ export default function SingleMezcalPage({ data: { project } }) {
         const { className, onClick } = props;
         return (
           <button className={className} onClick={onClick} onKeyDown={onClick}>
-            <img src='/arrow-l.png' alt='arrow button left'  />
+            <img src='/Left_ arrow.png' alt='arrow button left'  />
           </button>
         );
       }
@@ -48,11 +48,11 @@ export default function SingleMezcalPage({ data: { project } }) {
       };
 
 
-    const black = true
+    const black = false
 
   return (
     <Layout black={black}>
-        <ProjectContainer id='mezcales'>
+        <ProjectContainer id='project'>
             <div className="textoseo">
                 <h1>{project.title}</h1>
                 <h2>
@@ -94,11 +94,97 @@ export default function SingleMezcalPage({ data: { project } }) {
 
 
 const ProjectContainer = styled.section`
+position: absolute;
+top: 0;
+left: 0;
+right: 0;
+bottom: 0;
+background-color: black;
+.textoseo {
+  padding-top: 100px;
+  position: absolute;
+  width: 100%;
+}
  `
 const SliderContainer = styled(Slider)`
+position: relative;
+top: 0;
+height: 100vh;
+padding-top: 70px;
+.slick-arrow {
+    position: absolute;
+    z-index: 1;
+    bottom: 0px;
+    transform: translateY(-50%);
+    img {
+        width: 25px;
+        @media (max-width: 730px) {
+          width: 15px;
+        }
+    }
+}
+.slick-next {
+    right: 20px;
+    @media (max-width: 730px) {
+      right: 10px;
+    }
+  }
+.slick-prev {
+    left: 20px;
+    @media (max-width: 730px) {
+      left: 10px;
+    }
+}
+
+.slick-dots {
+    position: absolute;
+    bottom: 30px;
+    display: flex !important;
+    justify-content: center;
+    align-items: center;
+    height: 2px;
+    width: 50%;
+    margin: 0 auto;
+    left: 50%;
+    transform: translateX(-50%);
+    list-style: none;
+    @media (max-width: 862px) {
+      bottom: 30px;
+    }
+    @media (max-width: 730px) {
+      width: 80%;
+    }
+}
+.slick-dots li button:before, .slick-dots li button:after {
+    display: none !important;
+}
+.slick-dots li {
+    width: 14px;
+    height: 14px;
+    background-color: var(--white);
+    margin: 0 10px;
+    border-radius: 50%;
+    list-style: none;
+}
+
+.slick-dots li button {
+    background-color: var(--black);
+    border-radius: 50%;
+    border: solid 2px var(--white);
+    width: 10px;
+    height: 10px;
+}
+.slick-dots li button span {
+  display: none;
+}
+.slick-dots li.slick-active button {
+    background-color: var(--white);
+}
 `
 
 const Slide = styled.div`
+position: relative;
+height: auto;
 `
 
 export const query = graphql`
