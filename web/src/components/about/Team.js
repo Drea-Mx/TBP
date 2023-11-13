d02fab66e5d9ae081c03740f2578e97995ff49c7
Ximena <hola@ximena.dev>
2023-11-13 08:58:52 -0600

    team slider mobile

--- web/src/components/about/Team.js
+++ web/src/components/about/Team.js
@@ -1,13 +1,75 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import styled from 'styled-components'
 import BlockContent from '@sanity/block-content-to-react';
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import Slider from "react-slick"
 import scrollTo from 'gatsby-plugin-smoothscroll';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { localize } from "../../utils/helpers";
+
+function NextArrow(props) {
+    const { className, onClick } = props;
+  
+    return (
+      <div
+        className={className}
+        onClick={onClick}
+      >
+        <svg width="27" height="20" viewBox="0 0 27 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g clipPath="url(#clip0_47_1168)">
+          <path d="M2.28839e-06 10H25.7759" stroke="#FFF" strokeWidth="2" strokeMiterlimit="10"/>
+          <path d="M16.5141 19.3677L25.7759 10L16.5141 0.632324" stroke="#FFF" strokeWidth="2" strokeMiterlimit="10"/>
+          </g>
+          <defs>
+          <clipPath id="clip0_47_1168">
+            <rect width="27" height="20" fill="white" transform="matrix(-1 0 0 1 27 0)"/>
+          </clipPath>
+          </defs>
+        </svg>
+      </div>
+    );
+  }
+  
+  function PrevArrow(props) {
+    const { className, onClick } = props;
+    return (
+      <div
+        className={className}
+        onClick={onClick}
+      >
+        <svg width="27" height="20" viewBox="0 0 27 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g clipPath="url(#clip0_47_1245)">
+          <path d="M27 10H1.22412" stroke="#FFF" strokeWidth="2" strokeMiterlimit="10"/>
+          <path d="M10.4859 19.3677L1.22412 10L10.4859 0.632324" stroke="#FFF" strokeWidth="2" strokeMiterlimit="10"/>
+          </g>
+          <defs>
+          <clipPath id="clip0_47_1245">
+            <rect width="27" height="20" fill="white"/>
+          </clipPath>
+          </defs>
+        </svg>
+      </div>
+    );
+  }
 
 const Team = ({ data, language }) => {
     const text = localize(data.sanityAboutPage._rawOurTeamText2, [language])
     const top = localize(data.sanityAboutPage._rawToTop, [language])
+    const sliderRef = useRef(null);
+
+    const settings = {
+        dots: false,
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        waitForAnimate: true,
+        speed: 500,
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />,
+        ref: sliderRef,
+      };
 
 
     return(
@@ -45,6 +107,32 @@ const Team = ({ data, language }) => {
                         );
                     })}
                 </div>
+                <div className='team teamMobile'>
+                    <Slider {...settings}>
+                    {data.sanityAboutPage.team.map(({ _key, name, position2, image}, i) => {
+                        const bgGetDataImage = getImage(image.asset)
+                        const bgGetDataImageAlt = image.alt || ""
+                            return (
+                                <div className='member' key={_key}>
+                                    <div className='image'>
+                                        <GatsbyImage
+                                            style={{ height: "auto", width: "100%" }}
+                                            image={bgGetDataImage}
+                                            alt={bgGetDataImageAlt}
+                                        />
+                                    </div>
+                                    <div className='text'>
+                                        <h2>{name}</h2>
+                                        <div className='line'></div>
+                                        <h3><strong>{position2.translate}</strong></h3>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </Slider>
+                    <div className='arrows'>
+                    </div>
+                </div>
                 <div className='more'>
                     <button onClick={() => scrollTo('#home')} >
                         <BlockContent
@@ -63,6 +151,7 @@ position: relative;
 
 @media (max-width: 680px) {
     padding-top: 0;
+    padding-bottom: 2rem;
 }
     .text {
         background-color: var(--black);
@@ -94,6 +183,7 @@ position: relative;
         background-color: var(--black);
         @media (max-width: 680px) {
             grid-template-columns: repeat(1, 1fr);
+            display: none;
         }
         .member {
             position: relative;
@@ -104,7 +194,7 @@ position: relative;
                     transform: scale(1.05);
                 }
                 @media (max-width: 680px) {
-                    display: none;
+                    margin-bottom: 1rem;
                 }
             }
             &:hover {
@@ -156,6 +246,27 @@ position: relative;
                     font-family: var(--plain);
                 }
             }
+        }
+    }
+
+    .teamMobile {
+        display: none;
+        @media screen and (max-width: 680px) {
+            display: block;
+            padding: 0 3.5rem;
+        }
+
+        .slick-next:before, .slick-prev:before {
+            display: none;
+        }
+
+        .slick-arrow {
+            transform: translateY(15rem);
+        }
+
+        .slick-disabled {
+            opacity: 0;
+            pointer-events: none;
         }
     }
     .more {
