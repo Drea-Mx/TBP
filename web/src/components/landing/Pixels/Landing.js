65b9b75b97f33b4fefed8a87f877fc8d2afcaf0d
Ximena <hola@ximena.dev>
2024-03-11 01:29:19 -0600

    test wrapper pixel nativery

--- web/src/components/landing/Pixels/Landing.js
+++ web/src/components/landing/Pixels/Landing.js
@@ -1,84 +1,41 @@
-// components/TrackingPixels.js
-// import React from "react";
-// import Helmet from 'react-helmet'
-
-
-// export const renderLandingPixel = () => {
-//   const scripts = [];
-//   scripts.push(
-//     <script key="function" id="facebook-pixel-script">
-//       {`
-//         var axel = Math.random() + "";
-//         var a = axel * 10000000000000;
-//         document.write('<iframe src="https://14255851.fls.doubleclick.net/activityi;src=14255851;type=pagev0;cat=pixel0;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;ord=' + a + '?" width="1" height="1" frameborder="0" style="display:none"></iframe>');
-//       `}
-//     </script>
-//   );
-//   scripts.push(
-//     <noscript key="image" id="facebook-pixel-image">
-//       {`
-//         <iframe src="https://14255851.fls.doubleclick.net/activityi;src=14255851;type=pagev0;cat=pixel0;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;ord=1?" width="1" height="1" frameborder="0" style="display:none"></iframe>
-//       `}
-//     </noscript>
-//   );
-
-//   return scripts;
-// };
-
-// const PixelsLanding = () => (
-//   <Helmet>
-//     {/* Display Pixel */}
-//     <script type="text/javascript">
-//       {`
-//         var axel = Math.random() + "";
-//         var a = axel * 10000000000000;
-//         document.write('<iframe src="https://14255851.fls.doubleclick.net/activityi;src=14255851;type=pagev0;cat=pixel0;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;ord=' + a + '?" width="1" height="1" frameborder="0" style="display:none"></iframe>');
-//       `}
-//     </script>
-//     <noscript>
-//       {`
-//         <iframe src="https://14255851.fls.doubleclick.net/activityi;src=14255851;type=pagev0;cat=pixel0;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;ord=1?" width="1" height="1" frameborder="0" style="display:none"></iframe>
-//       `}
-//     </noscript>
-
-//     {/* Native Pixel */}
-//     <script type="text/javascript">
-//       {`
-//         var _nattp = _nattp || [];
-//         _nattp.push(['id', '65d36f84c6436d660ae06785',{nat_name: 'session_relevant'}]);
-//         (function() {
-//           var nattp = document.createElement('script'); nattp.type = 'text/javascript';
-//           nattp.src = '//cdn.nativery.com/widget/js/nattp.js';
-//           var nattps = document.getElementsByTagName('script')[0]; nattps.parentNode.insertBefore(nattp, nattps);
-//         })();
-//       `}
-//     </script>
-//     <noscript>
-//       {`<img height="1" width="1" src="//w.nativery.com/tp.gif?id=65d36f84c6436d660ae06785&nat_name=session_relevant" />`}
-//     </noscript>
-//   </Helmet>
-// );
-
-// export default PixelsLanding;
-
 import React from "react";
 
 const LandingPixel = () => {
   const renderMetaPixel = () => {
     const scripts = [];
     scripts.push(
-      <script key="function" id="facebook-pixel-script">
+      <script type="text/javascript">
         {`
           var axel = Math.random() + "";
           var a = axel * 10000000000000;
-          document.write('<iframe src="https://14255851.fls.doubleclick.net/activityi;src=14255851;type=pagev0;cat=pixel0;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;ord=' + a + '?" width="1" height="1" frameborder="0" style="display:none"></iframe>');
+          document.write('<iframe src="https://14255851.fls.doubleclick.net/activityi;src=14255851;type=pagev0;cat=pixel0;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=' + a + '?" width="1" height="1" frameborder="0" style="display:none"></iframe>');
         `}
       </script>
     );
     scripts.push(
-      <noscript key="image" id="facebook-pixel-image">
+      <noscript>
         {`
-          <iframe src="https://14255851.fls.doubleclick.net/activityi;src=14255851;type=pagev0;cat=pixel0;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;ord=1?" width="1" height="1" frameborder="0" style="display:none"></iframe>
+          <iframe src="https://14255851.fls.doubleclick.net/activityi;src=14255851;type=pagev0;cat=pixel0;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?" width="1" height="1" frameborder="0" style="display:none"></iframe>
+        `}
+      </noscript>
+    );
+    scripts.push(
+      <script type="text/javascript">
+      {`
+        var _nattp = _nattp || [];
+        _nattp.push(['id', '65d36f84c6436d660ae06785',{nat_name: 'session_relevant'}]);
+        (function() {
+          var nattp = document.createElement('script'); nattp.type = 'text/javascript';
+          nattp.src = '//cdn.nativery.com/widget/js/nattp.js';
+          var nattps = document.getElementsByTagName('script')[0]; nattps.parentNode.insertBefore(nattp, nattps);
+        })();
+       `}
+     </script>
+    );
+    scripts.push(
+      <noscript>
+        {`
+          <img height="1" width="1" src="//w.nativery.com/tp.gif?id=65d36f84c6436d660ae06785&nat_name=session_relevant" />
         `}
       </noscript>
     );
