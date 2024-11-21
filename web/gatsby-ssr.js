6ddd8444c3ac4c1fc884bc6644db4a7877436c85
Luis B. <luisbttf@gmail.com>
2024-11-21 07:33:06 -0600

    ssr2

--- web/gatsby-ssr.js
+++ web/gatsby-ssr.js
@@ -3,8 +3,6 @@ const wrapWithI18nProvider = require("./src/components/wrapWithI18nProvider")
 exports.wrapPageElement = wrapWithI18nProvider
 
 
-export const onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([
-    <script src="https://app.enzuzo.com/scripts/cookiebar/3ab108a2-a794-11ef-9d82-ff6eec0b7a18"></script>
-  ])
-}
+setHeadComponents([
+  <script src="https://app.enzuzo.com/scripts/cookiebar/3ab108a2-a794-11ef-9d82-ff6eec0b7a18"></script>,
+])
