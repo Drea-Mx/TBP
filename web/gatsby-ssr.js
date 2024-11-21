8725c920040cd13d61f6f24e93332a189212c9f0
Luis B. <luisbttf@gmail.com>
2024-11-21 07:34:47 -0600

    no ssr

--- web/gatsby-ssr.js
+++ web/gatsby-ssr.js
@@ -3,6 +3,3 @@ const wrapWithI18nProvider = require("./src/components/wrapWithI18nProvider")
 exports.wrapPageElement = wrapWithI18nProvider
 
 
-setHeadComponents([
-  <script src="https://app.enzuzo.com/scripts/cookiebar/3ab108a2-a794-11ef-9d82-ff6eec0b7a18"></script>,
-])
