853f1700518319e145d2ce3e511722fdc2289d16
Luis B. <luisbttf@gmail.com>
2024-11-22 08:26:23 -0600

    ssr4

--- web/gatsby-ssr.js
+++ web/gatsby-ssr.js
@@ -1,3 +1,18 @@
+import React from 'react';
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <script
+      key="enzuzo-cookiebar"
+      src="https://app.enzuzo.com/scripts/cookiebar/3ab108a2-a794-11ef-9d82-ff6eec0b7a18"
+    />,
+  ]);
+};
+
+
+
+
+
 const wrapWithI18nProvider = require("./src/components/wrapWithI18nProvider")
   .wrapWithI18nProvider
 exports.wrapPageElement = wrapWithI18nProvider
