01f06219ecc8b98bfbf6a703c7bf03ed7202376e
Luis B. <luisbttf@gmail.com>
2024-11-25 12:51:25 -0600

    iubenda

--- web/gatsby-ssr.js
+++ web/gatsby-ssr.js
@@ -7,11 +7,41 @@ const React = require("react");
 
 exports.onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
-    // Primer script (cookiebar)
+    // Primer script de configuración de Iubenda
     <script
-      key="enzuzo-cookiebar"
-      src="https://app.enzuzo.com/scripts/cookiebar/3ab108a2-a794-11ef-9d82-ff6eec0b7a18"
+      key="iubenda-config"
+      type="text/javascript"
+      dangerouslySetInnerHTML={{
+        __html: `
+          var _iub = _iub || [];
+          _iub.csConfiguration = {
+            "siteId": 3721072,
+            "cookiePolicyId": 62417021,
+            "lang": "es",
+            "storage": {"useSiteId": true}
+          };
+        `,
+      }}
+    />,
+    // Script de autoblocking
+    <script
+      key="iubenda-autoblocking"
+      type="text/javascript"
+      src="https://cs.iubenda.com/autoblocking/3721072.js"
+    />,
+    // Script de GPP
+    <script
+      key="iubenda-gpp"
+      type="text/javascript"
+      src="//cdn.iubenda.com/cs/gpp/stub.js"
+    />,
+    // Script de Iubenda Cookie Solution
+    <script
+      key="iubenda-cs"
+      type="text/javascript"
+      src="//cdn.iubenda.com/cs/iubenda_cs.js"
+      charset="UTF-8"
       async
-    />
+    />,
   ]);
 };
