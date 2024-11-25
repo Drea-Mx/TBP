bc88d201dcb5b93ad6acc556acec6312b03357d0
Luis B. <luisbttf@gmail.com>
2024-11-25 12:16:03 -0600

    cookie policy

--- web/gatsby-ssr.js
+++ web/gatsby-ssr.js
@@ -7,9 +7,18 @@ const React = require("react");
 
 exports.onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
+    // Primer script (cookiebar)
     <script
       key="enzuzo-cookiebar"
       src="https://app.enzuzo.com/scripts/cookiebar/3ab108a2-a794-11ef-9d82-ff6eec0b7a18"
+      async
+    />,
+    // Segundo script y div (cookies)
+    <div key="enzuzo-root" id="__enzuzo-root"></div>,
+    <script
+      key="enzuzo-root-script"
+      id="__enzuzo-root-script"
+      src="https://app.enzuzo.com/scripts/cookies/3ab108a2-a794-11ef-9d82-ff6eec0b7a18"
       async
     />,
   ]);
