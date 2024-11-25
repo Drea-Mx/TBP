c89b40ecc8d43a8e212462d5a8c120641b98abbc
Luis B. <luisbttf@gmail.com>
2024-11-25 11:15:28 -0600

    enzuzo

--- web/gatsby-ssr.js
+++ web/gatsby-ssr.js
@@ -3,3 +3,14 @@ const wrapWithI18nProvider = require("./src/components/wrapWithI18nProvider")
 exports.wrapPageElement = wrapWithI18nProvider
 
 
+const React = require("react");
+
+exports.onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <script
+      key="enzuzo-cookiebar"
+      src="https://app.enzuzo.com/scripts/cookiebar/3ab108a2-a794-11ef-9d82-ff6eec0b7a18"
+      async
+    />,
+  ]);
+};
--- web/src/components/layout/layout.js
+++ web/src/components/layout/layout.js
@@ -3,15 +3,12 @@ import { Normalizer } from "../style/Normalizer"
 import { GlobalStyle } from "../style/GlobalStyle"
 import Header from './header'
 import Footer from "./footer"
-import { Helmet } from 'react-helmet';
 
 function Layout({ children, pageContext }) {
   if (pageContext.layout === "landing") {
     return (
       <>
-        <Helmet>
-          <script src="https://app.enzuzo.com/scripts/cookiebar/3ab108a2-a794-11ef-9d82-ff6eec0b7a18"></script>
-        </Helmet>
+
         <Normalizer />
         <GlobalStyle />
         <main >{children}</main>
